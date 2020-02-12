473772d4ad78b07da9acc17b315180cab29fdd80
Lakindu Akash <lakinduakash@gmail.com>
2020-02-12 12:18:52 +0530

    Add prometheus::server

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,9 +2,49 @@
 #
 #
 class prometheus_install {
-  # resources
-  package { 'ntp':
-      ensure => installed,
-    }
+
+  class { 'prometheus::server':
+  version        => '2.4.3',
+  alerts         => {
+    'groups' => [
+      {
+        'name'  => 'alert.rules',
+        'rules' => [
+          {
+            'alert'       => 'InstanceDown',
+            'expr'        => 'up == 0',
+            'for'         => '5m',
+            'labels'      => {
+              'severity' => 'page',
+            },
+            'annotations' => {
+              'summary'     => 'Instance {{ $labels.instance }} down',
+              'description' => '{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 5 minutes.'
+            }
+          },
+        ],
+      },
+    ],
+  },
+  scrape_configs => [
+    {
+      'job_name'        => 'prometheus',
+      'scrape_interval' => '10s',
+      'scrape_timeout'  => '10s',
+      'static_configs'  => [
+        {
+          'targets' => [ 'localhost:9090' ],
+          'labels'  => {
+            'alias' => 'Prometheus',
+          }
+        }
+      ],
+    },
+  ],
+}
+
+class { 'prometheus::node_exporter':
+  version            => '0.12.0',
+}
 
 }
