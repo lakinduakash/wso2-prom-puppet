4d4ee994bdd9252958aea671045acc2429bcd6ba
Lakindu Akash <lakinduakash@gmail.com>
2020-02-13 15:26:40 +0530

    config alert manager

--- manifests/init.pp
+++ manifests/init.pp
@@ -59,6 +59,30 @@ class prometheus_install {
   ],
 }
 
+class { 'prometheus::alertmanager':
+  version   => '0.13.0',
+  route     => {
+    'group_by'        => ['alertname', 'cluster', 'service'],
+    'group_wait'      => '30s',
+    'group_interval'  => '5m',
+    'repeat_interval' => '3h',
+    'receiver'        => 'slack',
+  },
+  receivers => [
+    {
+      'name'          => 'slack',
+      'slack_configs' => [
+        {
+          'api_url'       => 'https://hooks.slack.com/services/ABCDEFG123456',
+          'channel'       => '#channel',
+          'send_resolved' => true,
+          'username'      => 'username'
+        },
+      ],
+    },
+  ],
+}
+
 class { 'prometheus::node_exporter':
   version            => '0.18.0',
 }
